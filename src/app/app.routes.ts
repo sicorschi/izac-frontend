56b727031122a37860c2f0b32aef0855223b4244
Igor Sicorschi <igor.sicorschi@gmail.com>
2026-09-01 03:05:02 +0200

    add new weather cast section at dashboard panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,36 +1,33 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.DashboardComponent),
-    },
-     {
-        path: 'devices',
-        loadComponent: () => import('./pages/devices/devices').then((m) => m.DevicesComponent),
-    },
-     {
-        path: 'sensors',
-        loadComponent: () => import('./pages/sensors/sensors').then((m) => m.SensorsComponent),
-    },
-     {
-        path: 'infrastructures',
-        loadComponent: () => import('./pages/infrastructures/infrastructures').then((m) => m.InfrastructuresComponent),
-    },
-     {
-        path: 'builders',
-        loadComponent: () => import('./pages/builders/builders').then((m) => m.BuildersComponent),
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./pages/login/login').then((m) => m.LoginComponent),
-    },
-    {
-        path: 'not-found',
-        loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFoundComponent),
-    },
-    {
-        path: '**',
-        redirectTo: 'not-found',
-    }
+  {
+    path: '',
+    loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.DashboardComponent),
+  },
+  {
+    path: 'devices',
+    loadComponent: () => import('./pages/devices/devices').then((m) => m.DevicesComponent),
+  },
+  {
+    path: 'infrastructures',
+    loadComponent: () =>
+      import('./pages/infrastructures/infrastructures').then((m) => m.InfrastructuresComponent),
+  },
+  {
+    path: 'builders',
+    loadComponent: () => import('./pages/builders/builders').then((m) => m.BuildersComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'not-found',
+    loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFoundComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  },
 ];
--- src/app/models/sensors/create-request.types.ts
+++
@@ -1,6 +0,0 @@
-export interface CreateSensorRequest {
-  name: string;
-  type: string;
-  unit: string;
-  threshold: number;
-}
